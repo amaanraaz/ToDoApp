d0b08675af37ea7f9baeb0d6752ccea2edbe3978
amaanraaj <amandwivedi00@gmail.com>
2022-12-02 02:33:42 +0530

    Inital Commit,Just completed with help of react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css'
 
 function App() {
+  const [todoList,settodoList] = useState([]);
+  const [currentTask,setCurrentTask] = useState("");
+  const [val,setVal] = useState('');
+
   return (
+    <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <div className='todo'>
+
+        <input type='text' placeholder='Enter task here...' value={val}
+          onChange={(event)=>{
+            setCurrentTask(event.target.value);
+            setVal(event.target.value);
+            
+            }}
+            onKeyDown={(event)=>{
+              if(event.keyCode==13){
+                settodoList([...todoList,currentTask]);
+                setVal('');
+              }
+            }}
+            ></input>
+
+        <button onClick={()=>{
+          settodoList([...todoList,currentTask]);
+          setVal('');
+        }}>Add</button>
+
+      </div>
+    </div>
+      <hr className='line'/>
+      <ul className='taskList'>
+        {
+            todoList.map((val,key)=>{
+              return (
+                  <div className='tasks-list'>
+                    <li>{val}</li>
+                    <button onClick={
+                      ()=>{
+                        settodoList(
+                        todoList.filter((item)=>{
+                          return item!==val
+                        }))
+                      }
+                    }>Delete</button>
+                  </div>
+              )
+            })
+        }
+      </ul>
     </div>
   );
 }
